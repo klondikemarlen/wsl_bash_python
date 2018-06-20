55b6638986569bdb73bb69dc7603c6445e6e8712
Marlen Brunner <klondikemarlen@gmail.com>
2018-06-20 13:29:39 -0700

    Attempting to understand subproccess + PIPES.
    I'm totally confused.
    I feel like I can't work out what code is executing when? I can't quite seem to print any data from standard output except after the program has executed.

--- source/python.py
+++ source/python.py
@@ -1,15 +1,19 @@
 import subprocess
 import sys
+from pprint import pprint
+import pdb
+import shlex
 
 
 def convert_to_linux_path(path):
-    print("Before convert:", repr(path))
+    # print("Before convert:", repr(path))
     path = path.replace("\\", "/")  # Must come before the rest.
     path = path.replace("C:", "/mnt/c")
     path = path.replace(" ", "\ ")
     path = path.replace("(", "\\(")
     path = path.replace(")", "\\)")
-    print("After convert:", repr(path))
+    # path = shlex.quote(path)
+    # print("After convert:", repr(path))
     return path
 
 
@@ -17,11 +21,40 @@ def convert_all_paths_to_linux(paths):
     return [convert_to_linux_path(path) for path in paths]
 
 
+def start_interpreter(cmd):
+    with subprocess.Popen("bash", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True) as proc:
+        # proc.stdin.write(cmd)
+        # proc.stdin.flush()
+        # with proc.stdout as f:
+        #     print(f.read())
+
+        # print("Foo:", foo)
+        try:
+            outs, errs = proc.communicate(cmd)
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            outs, errs = proc.communicate()
+        # while True:
+        #     with proc.stdin as f:
+        #         line = proc.stdin.readline()
+        #         if line == "exit()":
+        #             break
+        #         outs, errs = proc.communicate(line)
+        #         if outs:
+        #             pprint(outs)
+        #         if errs:
+        #             pprint(errs)
+
+        print("Output:")
+        pprint(outs)
+        print("Errors:")
+        pprint(errs)
+
+
 if __name__ == "__main__":
     print("System arguments:", sys.argv)
     args = convert_all_paths_to_linux(sys.argv[1:])
     print("Converted args:", args)
-    cmd = "bash -c 'python3 {}'".format(' '.join(args))
-    print(cmd)
-    process = subprocess.run(cmd, stderr=subprocess.STDOUT)
-    print(process)
+    cmd = 'python3 {}'.format(' '.join(args))
+    print("Command:", cmd)
+    start_interpreter(cmd)
--- source/test_bash_python.py
+++ source/test_bash_python.py
@@ -1,6 +1,9 @@
+import python
+
 fake_input = "C:\Program Files (x86)\JetBrains\PyCharm Community Edition 2018.1.4\helpers\pydev\pydevconsole.py"
 
-import python
 print(repr(fake_input))
 
 assert python.covert_to_linux_path(fake_input).startswith("/mnt/c/Program\ Files\ \(x86\)/")
+
+fake_input2 = "import sys; print('Python %s on %s' % (sys.version, sys.platform))"
